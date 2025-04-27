51a3bb3d526aeb76ab1fe9ab1963bb47542a9a92
Karen Poghosyan <karenpoghosyan242@gmail.com>
2025-04-27 14:33:48 +0400

    Added redmi.md updata index.html

---
+++ README.md
@@ -0,0 +1,25 @@
+# Django project Svarto
+## Clone repo.
+```
+git clone https://github.com/Karen042009/Svarto.git
+```
+## Creation of virtual environments 
+```
+python -m venv venv
+.\venv\Scripts\activate
+```
+## Install Django
+```
+pip install Django
+django-admin --version
+```
+## Run server. 
+```
+cd my_biography
+python manage.py migrate  
+python manage.py runserver
+```
+## Request
+```
+127.0.0.1:8000/main/
+```
---
+++ Svarto/main/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+
+class Class(models.Model):
+    class_name = models.CharField(max_length=20)
+    teachers = models.ManyToManyField(Teacher, related_name='classes')
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    class_id = models.ForeignKey(Class, on_delete=models.CASCADE, related_name='students')
+
+class Subject(models.Model):
+    subject_name = models.CharField(max_length=50)
+
+class Gradebook(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='gradebooks')
+
+class Grade(models.Model):
+    gradebook = models.ForeignKey(Gradebook, on_delete=models.CASCADE, related_name='grades')
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='grades')
+    grade = models.IntegerField()
