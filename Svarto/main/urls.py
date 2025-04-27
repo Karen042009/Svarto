7a3dfd481a25aa5c774886824b925e3e52178285
Karen Poghosyan <karenpoghosyan242@gmail.com>
2025-04-27 17:32:41 +0400

    Updata index.html and fetch

--- Svarto/main/urls.py
+++ Svarto/main/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('main/', views.main, name='main'),
     path('teachers/', views.teacher_list, name='teacher_list'),
     path('teachers/json/', views.get_teachers, name='get_teachers'),
+    path('students/', views.student_list, name='student_list'), 
+    path('students/json/', views.get_students, name='get_students'),
+    path('classes/', views.class_list, name='class_list'),  
+    path('classes/json/', views.get_classes, name='get_classes'), 
     path('admin/', admin.site.urls),
 ]
--- Svarto/main/views.py
+++ Svarto/main/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
 from django.shortcuts import render
-from .models import Teacher
+from .models import Teacher, Student, Class
 
 def main(request):
     template = loader.get_template("index.html")
@@ -16,7 +16,22 @@ def get_teachers(request):
     teacher_list = [{"name": f"{teacher.first_name} {teacher.last_name}"} for teacher in teachers]
     return JsonResponse(teacher_list, safe=False)
 
-def create_teachers(request):
-    Teacher.objects.create(first_name="Կարեն", last_name="Պետրոսյան")
-    Teacher.objects.create(first_name="Անի", last_name="Հովհաննիսյան")
-    return HttpResponse("Teachers have been added.")
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, 'student_list.html', {'students': students})
+
+def get_students(request):
+    students = Student.objects.all()
+    student_list = [{"name": f"{student.first_name} {student.last_name}", "class": student.class_id.class_name} for student in students]
+    return JsonResponse(student_list, safe=False)
+
+def class_list(request):
+    classes = Class.objects.all()
+    return render(request, 'class_list.html', {'classes': classes})
+
+def get_classes(request):
+    classes = Class.objects.all()
+    class_list = [{"class_name": class_obj.class_name, 
+                   "students": [f"{student.first_name} {student.last_name}" for student in class_obj.students.all()]} 
+                  for class_obj in classes]
+    return JsonResponse(class_list, safe=False)
